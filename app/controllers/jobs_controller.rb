4829383ab64918df9759a43217a48b772f35c55f
Guilherme e Marcio <lguilherme.santos@live.com>
2016-07-11 20:37:48 -0300

    Usuário edita vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,14 +1,15 @@
 class JobsController < ApplicationController
 	def new
 		@job = Job.new
+		@companies = Company.all
 	end
 
 	def create
 		@job = Job.new(job_params)
-		if @job.valid?
-			@job.save
+		if @job.save
 			redirect_to @job
 		else
+			@companies = Company.all
 			flash[:notice] = "Não foi possível criar a vaga"
 			render 'new'
 		end
@@ -16,6 +17,22 @@ class JobsController < ApplicationController
 
 	def show
 		@job = Job.find(params[:id])
+	end
+
+	def edit
+		@job = Job.find(params[:id])
+		@companies = Company.all
+	end
+
+	def update
+		@job = Job.find(params[:id])
+		if @job.update(job_params)
+			redirect_to @job
+		else
+			@companies = Company.all
+			flash[:notice] = "Não foi possível atualizar a vaga"
+			render 'edit'
+		end
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 	root 'home#index'
-	resources :jobs, only: [:show, :new, :create]
+	resources :jobs, only: [:show, :new, :create, :edit, :update]
 	resources :companies, only: [:show, :new, :create]
 
 end
